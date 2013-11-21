a2c6cb5b2f5a10300f5256adeec619ecc8387f8e
Tobias Bielohlawek <tobi@soundcloud.com>
2013-11-21 18:24:39 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlaymakersApp::Application.config.secret_key_base = '9e441a8652f57726b1ed84c09748a276f80166c26d6801022063997343d6bda7b93f9a082d27ed86e119fd8ba75dc47132c2b349ba6f140d5f9aa00d175bf1c1'
