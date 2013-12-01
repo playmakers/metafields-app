4b15cf34761728777d76c4d291649a0f3c102799
Tobias Bielohlawek <tobi@soundcloud.com>
2013-12-01 19:11:19 +0100

    update default data

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,37 +1,42 @@
 #encoding: utf-8
 
 FITTING_HELMET_1 = "<h3>Nimm deine Kopfmaße</h3>
-Messe mit einem Maßband deinen Kopfumfang etwa 2,5cm über deinen Augenbrauen. Falls du kein Maßband zur Hand hast, nimm einfach eine Schnur, markiere die Stellen an der sie sich kreuzt und messe den Abstand.
+Messe mit einem Maßband deinen Kopfumfang etwa 2,5 cm über deinen Augenbrauen. Falls du kein Maßband zur Hand hast, nimm einfach eine Schnur, markiere die Stellen an der sie sich kreuzt und messe den Abstand.
 Vergleiche den gemessenen Wert mit der nebenstehenden Tabelle, um deine Helmgröße zu bestimmen."
 
+FITTING_HELMET_2A = "Helmgröße;Kopfumfang
+M;51 cm - 56 cm
+L;56 cm - 59 cm
+XL;ab 59 cm"
+
 FITTING_HELMET_2B = "Helmgröße;Kopfumfang
-S;48cm - 52cm
-M;52cm - 56cm
-L;56cm - 60cm
-XL;ab 60cm"
+S;48 cm - 52 cm
+M;52 cm - 56 cm
+L;56 cm - 60 cm
+XL;ab 60 cm"
 
 FITTING_PAD_1 = "<h3>Nimm deine Schulter- und Brustmaße</h3>
 Messe mit einem Maßband den Abstand von der Spitze deines linken Oberarmknochens zur Spitze deines rechten Oberarmknochens. Danach messe mit dem Maßband deinen Brustumfang auf Höhe deiner Brustwarzen. Falls du kein Maßband zur Hand hast, nimm einfach eine Schnur, markiere die Stellen an der sie sich kreuzt und messe den Abstand.
 Vergleiche die gemessenen Werte mit der nebenstehenden Tabelle, um die Größe für dein Schulterpolster zu bestimmen"
 
 FITTING_PAD_2 = "Padgröße;Schulterbreite;Brustumfang
-S  (17-18‘‘);28cm - 30,5cm;66cm – 71cm
-M (18-19‘‘);30,5cm – 33cm;71cm – 76cm
-L (19-20‘‘);33cm - 35,5cm;76cm – 81cm
-XL (20-21‘‘);35,5cm – 38cm;81cm – 86cm
-XXL (21-22‘‘);38cm - 40,5cm;91cm – 96cm
-XXXL (22-23‘‘);ab 40,5cm ab;96cm"
+S  (17-18\");28 cm - 30,5 cm;66 cm – 71 cm
+M (18-19\");30,5 cm – 33 cm;71 cm – 76 cm
+L (19-20\");33 cm - 35,5 cm;76 cm – 81 cm
+XL (20-21\");35,5 cm – 38 cm;81 cm – 86 cm
+XXL (21-22\");38 cm - 40,5 cm;91 cm – 96 cm
+XXXL (22-23\");ab 40,5 cm ab;96 cm"
 
 FITTING_NIKE_1 = "Die richtige Größe für Nike Footballhandschuhe lassen sich durch den Umfang deiner Hand bestimmen. Um die für dich richtige Größe zu ermitteln, messe mit einem Maßband den Umfang deiner Hand auf Höhe deiner Fingerknöchel. Falls du kein Maßband zur Hand hast, nimm einfach eine Schnur, markiere die Stellen an der sie sich kreuzt und messe den Abstand.
 Vergleiche den gemessenen Wert mit der nebenstehenden Tabelle, um deine Nike Handschuhgröße zu bestimmen."
 
 FITTING_NIKE_2 = "
 Handschuhgröße;Handumfang
-S;18cm – 19cm
-M;19cm - 19,5cm
-L;19,5cm - 20,5cm
-XL;20,5cm – 21,5cm
-XXL;ab 21,5cm"
+S;18 cm – 19 cm
+M;19 cm - 19,5 cm
+L;19,5 cm - 20,5 cm
+XL;20,5 cm – 21,5 cm
+XXL;ab 21,5 cm"
 
 FITTING_CUTTERS = "Die richtige Größe für Cutters Footballhandschuhe lassen sich durch das nebenstehende Cutters Größendiagramm ermitteln. Klicke auf das Dokument, um es zu öffnen und drucke es auf DIN A4 Größe aus. Lege anschließend deine Hand auf das Größendiagramm und achte dabei darauf, dass das untere Ende deiner Handfläche genau mit der abgebildeten Hand abschließt. Lese nun ab welchen Kreis dein Mittelfinger erreicht, um deine Cutters Handschuhgröße zu bestimmen"
 
@@ -69,70 +74,51 @@ module ProductsHelper
 
   def defaults_hash
     {
-      # 163964023
+      163963715 => {
+        "features" => default_features('163963715'),
+        "fitting"  => [FITTING_HELMET_1, FITTING_HELMET_2A],
+      },
+      163964023 => {
+        "features" => default_features('163964023'),
+        "fitting"  => [FITTING_HELMET_1, FITTING_HELMET_2A],
+      },
       176259965 => {
         "fitting" => [FITTING_HELMET_1, FITTING_HELMET_2B],
       },
       #  EVOLUTION
-      163972709 => {
+      [186464681] => { #163972709, 163973197, 163973513
         "features"    => default_features('pad_evolution'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
-      },
-      163973197 => {
-        "features"    => default_features('pad_evolution'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
-      },
-      163973513 => {
-        "features"    => default_features('pad_evolution'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
+        # "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
         "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
       },
 
       # SPK
-      163970683 => {
+      [186467237] => { #163970683, 163969649, 163971035, 163970227
         "features"    => default_features('pad_spk'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
-      },
-      163969649 => {
-        "features"    => default_features('pad_spk'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
-      },
-      163971035 => {
-        "features"    => default_features('pad_spk'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
-      },
-      163970227 => {
-        "features"    => default_features('pad_spk'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
+        # "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
         "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
       },
 
       # SPX
-      176366809 => {
+      [186475501] => { #, 176366809, 176367113, 176367325, 176367533] => {
         "features"    => default_features('pad_spx'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
+        # "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
         "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
       },
-      176367113 => {
-        "features"    => default_features('pad_spx'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
+
+      # Hands
+      [164011635, 164013821, 164007995, 164009703]  => {
+        "fitting"     => [FITTING_CUTTERS],
       },
-      176367325 => {
-        "features"    => default_features('pad_spx'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
+      [164002313, 164001567, 164001371, 176330009, 164001469]  => {
+        "fitting"     => [FITTING_NIKE_1, FITTING_NIKE_2],
       },
-      176367533 => {
-        "features"    => default_features('pad_spx'),
-        "accessories" => ['180716833,180711969,180715553','180717561','180719109'],
-        "fitting"     => [FITTING_PAD_1, FITTING_PAD_2],
+
+      # Shoes
+      [179138937, 179142745, 179143693, 179141233]  => {
+        "fitting"     => [FITTING_SHOES],
       },
+
     }
   end
 
@@ -141,9 +127,18 @@ module ProductsHelper
   end
 
   def default_features_hash
-"163964023;png;Verlängerte Helmschale;Veränderte Schalenform für Schutz der Kieferpartie vor seitlichen Schlägen
+"
+163963715;png;Flex Impact System;Facemask und seitlich angebrachte Clips reduzieren die Wucht des frontalen Aufpralls
+163963715;png;Verlängerte Helmschale; Veränderte Schalenform für Schutz der Kieferpartie vor seitlichen Schlägen
+163963715;png;Ideal Fit Technologie;Optimale Anpassung an den Hinterkopf für zusätzlichen Schutz und verbesserten Halt
+163963715;png;Versetztes Schalendesign;Stärker versetzte Form für verbesserte Stablilität der Helmschale
+163963715;png;Innenpolsterung;Herausnehmbare Polsterung für hervorragenden Tragekompfort
+163963715;png;Light Weight Technologie;Leichtes Schalen- und Facemaskmaterial verbessern den Tragekompfort
+163963715;png;Aufblasbare Polsterung;Individuell aufblasbare Luftkammern sorgen für einen optimalen Sitz des Helms
+163963715;png;Quick-Release-Clips;Schnelle Demontage des Facemasks für direkten Zugang zum Spieler
+163964023;png;Verlängerte Helmschale;Veränderte Schalenform für Schutz der Kieferpartie vor seitlichen Schlägen
 163964023;png;Versetztes Schalendesign;Stärker versetzte Form für verbesserte Stablilität der Helmschale
-163964023;png;Innenpolsterung Herausnehmbare Polsterung für hervorragenden Tragekompfort
+163964023;png;Innenpolsterung;Herausnehmbare Polsterung für hervorragenden Tragekompfort
 163964023;png;Light Weight Technologie;Leichtes Schalen- und Facemaskmaterial verbessern den Tragekompfort
 163964023;png;Aufblasbare Polsterung;Individuell aufblasbare Luftkammern sorgen für einen optimalen Sitz des Helms
 163964023;png;Quick-Release-Clips;Schnelle Demontage der seitlichen Clips für direkten Zugang zum Spieler
@@ -164,3 +159,5 @@ pad_evolution;png;Flat Pad;Flaches Pad-Design für höhere Bewegungsfreiheit und
     end
   end
 end
+
+
