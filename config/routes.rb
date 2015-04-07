ad46c94e3b5c5ed283f0b082c4e809f22eed85bc
playmakers <info@play-makers.de>
2015-04-07 21:28:44 +0200

    remove variant generation

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,14 @@ PlaymakersApp::Application.routes.draw do
     delete 'logout'                => :destroy
   end
 
+  get  'import'   => 'products#update_variant_quantities'
+
   get 'products/show' => redirect { |params, req|
     "/products/#{req.query_parameters['id']}"
   }
 
-  get  'products/:id/variants/:variant_id/image.:format' => 'images#variant'
-
-  get  'import'   => 'products#update_variant_quantities'
-
   get  'products/:id' => 'products#show'
   get  'products/edit' => 'products#edit'
-
-  # get 'welcome' => 'home#welcome'
-  # get 'design'  => 'home#design'
-  # get  'images'               => 'images#show'
-  # get  'products/metafields'  => 'products#metafields'
-  # post 'products/metafields'  => 'products#metafields'
-
-
 
   root :to => 'home#index'
 end
